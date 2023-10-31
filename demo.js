f32ffd3a02d3db913357966902db734be59b9fc4
smallfawn <101914820+smallfawn@users.noreply.github.com>
2023-10-31 14:51:08 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,13 @@
-const fs = require('fs');
+const fs = require('fsconst path = require('path');
 
-// 创建demo.json文件
+// 仓根目录
+const rootDir = process.cwd();
+
+// 创建demo.json文件的绝对路径
+const demoFilePath = path.join(rootDir, 'demo.json');
+
+// 创建demo.json
 const data = { message: 'Hello, world!' };
-fs.writeFileSync('demo.json', JSON.stringify(data));
+fs.writeFileSync(demoFilePath, JSON.stringify(data));
 
 console.log('demo.json文件创建成功！');
